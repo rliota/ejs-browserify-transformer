7ee8314e2cb6c009ae1e8205876ba5f4bf1d5f28
rliota <robertliota.public@gmail.com>
2015-09-23 03:12:46 -0400

    re-worked wrap function

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+/**
+
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Robert Liota
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
+
 var ejs = require('ejs'),
     through = require('through'),
     path = require('path');
 
 
-function wrap(templateFn){
-    return 'module.exports=(function(){var t=' + templateFn + '; return function(l){ return t(l);}}())'; // Simen Brekken <simen@unfold.no>
+function wrap(compiled){
+    return 'module.exports=(function(){var compiled=' + compiled + '; return function(d){ return compiled(d);}}())';
 }
 
 
@@ -17,9 +43,6 @@ var transformerFactory = {
         if(opts.fileExtensions && opts.fileExtensions instanceof Array){
             fileExtensionsToTraverse.concat(opts.fileExtensions);
         }
-
-
-
 
         return function(fileName) {
             if (fileExtensionsToTraverse.indexOf(path.extname(fileName)) === -1){
